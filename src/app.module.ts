72c37cd933cf0ea1280a898409dd0989c364abb9
Kayk_Darios <manjac688@gmail.com>
2025-05-02 01:35:38 -0300

    chore: criando modulos basicos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { LocationsModule } from './locations/locations.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, LocationsModule, PostsModule],
   controllers: [],
   providers: [],
 })
