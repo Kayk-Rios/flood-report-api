7daa7172ce25037e80e77d9b3592147288375a6d
Kayk_Darios <manjac688@gmail.com>
2025-05-02 02:42:00 -0300

    chore: modulo location atualizado

--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { LocationsService } from './locations.service';
+import { LocationsResolver } from './locations.resolver';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Module({})
+@Module({
+  providers: [LocationsService, LocationsResolver, PrismaService]
+})
 export class LocationsModule {}
