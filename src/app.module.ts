5f506c905e82919f563c0ff6af70edd794d7974f
Kayk_Darios <manjac688@gmail.com>
2025-05-02 02:37:35 -0300

    feat: atualiznado o appModulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,21 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { LocationsModule } from './locations/locations.module';
 import { PostsModule } from './posts/posts.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule, UsersModule, LocationsModule, PostsModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      context: ({ req }) => ({ req }),
+    }),
+    AuthModule, UsersModule, LocationsModule, PostsModule, PrismaModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
