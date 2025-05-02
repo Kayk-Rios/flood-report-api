2b8f42f2d6a1feedf7b4290ce48a9edaa831d5fd
Kayk_Darios <manjac688@gmail.com>
2025-05-02 02:40:00 -0300

    chore: atualizando auth modulo

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersModule } from 'src/users/users.module';
 
-@Module({})
+@Module({
+  imports: [UsersModule],
+  providers: [AuthService, AuthResolver, PrismaService]
+})
 export class AuthModule {}
