0c82dd84686b8acb7a21479c17bf1bcdae54e970
Kayk_Darios <manjac688@gmail.com>
2025-05-20 23:05:59 -0300

    feat: methods usados

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AuthMiddleware } from './auth/auth.middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(new AuthMiddleware().use);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
