39fcb9351a5862f82aec87492ec7c4c6086d7aff
Kayk_Darios <manjac688@gmail.com>
2025-05-22 16:36:44 -0300

    feat: usando o a secret do jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -7,7 +8,13 @@ import { AuthGuard } from './auth.guard';
 import { AdminGuard } from './admin.guard';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+    UsersModule],
   providers: [AuthService, AuthResolver, PrismaService, AuthGuard, AdminGuard],
   exports: [AuthService, AuthGuard,AdminGuard],
 })
