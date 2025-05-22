4789e13b84e778dce01061d011c83fee6b23916f
Kayk_Darios <manjac688@gmail.com>
2025-05-22 16:39:28 -0300

    feat: realizando funcionalidades basicas de regioes como estados e  cidades realionada a estados

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,17 +1,26 @@
-
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { State } from './entities/state.entity';
 import { City } from './entities/city.entity';
-
-
+import { CreateCityInput } from './dto/create-city.input';
+import { CreateStateInput } from './dto/create-state.input';
+import { UpdateCityInput } from './dto/update-city.input';
+import { UpdateStateInput } from './dto/update-state.input';
 
 @Injectable()
 export class LocationsService {
   constructor(private prisma: PrismaService) {}
 
   async findAllStates(): Promise<State[]> {
-    return this.prisma.state.findMany();
+    return this.prisma.state.findMany({
+      include: {
+        cities: {
+          include: {
+            state: true,
+          },
+        },
+      },
+    });
   }
 
   async findCitiesByState(stateId: number): Promise<City[]> {
@@ -23,6 +32,48 @@ export class LocationsService {
 
   async findCityById(id: number): Promise<City | null> {
     return this.prisma.city.findUnique({
+      where: { id },
+      include: { state: true },
+    });
+  }
+
+  async createState(createStateInput: CreateStateInput): Promise<State> {
+    return this.prisma.state.create({
+      data: createStateInput,
+    });
+  }
+
+  async updateState(updateStateInput: UpdateStateInput): Promise<State> {
+    return this.prisma.state.update({
+      where: { id: updateStateInput.id },
+      data: updateStateInput,
+    });
+  }
+
+  async deleteState(id: number): Promise<State> {
+    return this.prisma.state.delete({
+      where: { id },
+    });
+  }
+
+  // Operações CRUD para Cidades
+  async createCity(createCityInput: CreateCityInput): Promise<City> {
+    return this.prisma.city.create({
+      data: createCityInput,
+      include: { state: true },
+    });
+  }
+
+  async updateCity(updateCityInput: UpdateCityInput): Promise<City> {
+    return this.prisma.city.update({
+      where: { id: updateCityInput.id },
+      data: updateCityInput,
+      include: { state: true },
+    });
+  }
+
+  async deleteCity(id: number): Promise<City> {
+    return this.prisma.city.delete({
       where: { id },
       include: { state: true },
     });
