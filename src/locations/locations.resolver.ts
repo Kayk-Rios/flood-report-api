e0a5dc8f6a9feb7a527028aeaa2cc4a8fc74499d
Kayk_Darios <manjac688@gmail.com>
2025-05-22 16:40:09 -0300

    feat: utilizando todas possiveis funçoes do service

--- src/locations/locations.resolver.ts
+++ src/locations/locations.resolver.ts
@@ -1,8 +1,12 @@
 
-import { Resolver, Query, Args, ID } from '@nestjs/graphql';
+import { Resolver, Query, Args, ID, Mutation } from '@nestjs/graphql';
 import { LocationsService } from './locations.service';
 import { State } from './entities/state.entity';
 import { City } from './entities/city.entity';
+import { CreateStateInput } from './dto/create-state.input';
+import { UpdateStateInput } from './dto/update-state.input';
+import { CreateCityInput } from './dto/create-city.input';
+import { UpdateCityInput } from './dto/update-city.input';
 
 @Resolver()
 export class LocationsResolver {
@@ -21,5 +25,37 @@ export class LocationsResolver {
   @Query(() => City, { nullable: true })
   city(@Args('id', { type: () => ID }) id: number) {
     return this.locationsService.findCityById(id);
+  }
+
+
+  @Mutation(() => State)
+  createState(@Args('createStateInput') createStateInput: CreateStateInput) {
+    return this.locationsService.createState(createStateInput);
+  }
+
+  @Mutation(() => State)
+  updateState(@Args('updateStateInput') updateStateInput: UpdateStateInput) {
+    return this.locationsService.updateState(updateStateInput);
+  }
+
+  @Mutation(() => State)
+  deleteState(@Args('id', { type: () => ID }) id: number) {
+    return this.locationsService.deleteState(id);
+  }
+
+  // Mutations para Cidades
+  @Mutation(() => City)
+  createCity(@Args('createCityInput') createCityInput: CreateCityInput) {
+    return this.locationsService.createCity(createCityInput);
+  }
+
+  @Mutation(() => City)
+  updateCity(@Args('updateCityInput') updateCityInput: UpdateCityInput) {
+    return this.locationsService.updateCity(updateCityInput);
+  }
+
+  @Mutation(() => City)
+  deleteCity(@Args('id', { type: () => ID }) id: number) {
+    return this.locationsService.deleteCity(id);
   }
 }
