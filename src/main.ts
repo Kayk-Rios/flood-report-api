c5ad0bdf5369e0f4b2b925c67c9f31df4f111d61
Kayk_Darios <manjac688@gmail.com>
2025-05-22 22:29:58 -0300

    fix: voltando as configuraçoes normais

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthMiddleware } from './auth/auth.middleware';
 import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth/auth.service';
-
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
 
+  const prismaService = app.get(PrismaService);
   const jwtService = app.get(JwtService);
-  const authService = app.get(AuthService); 
+  const authService = new AuthService(prismaService, jwtService);
   
   app.use(new AuthMiddleware(jwtService, authService).use);
   
