d68301f257fc878dfe01305779ef392bcbebbd72
Kayk_Darios <manjac688@gmail.com>
2025-05-22 22:06:15 -0300

    fix correçao instanciar manualmente o AuthService fora do contexto

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,14 @@ import { AppModule } from './app.module';
 import { AuthMiddleware } from './auth/auth.middleware';
 import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth/auth.service';
-import { PrismaService } from './prisma/prisma.service';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
 
-  const prismaService = app.get(PrismaService);
   const jwtService = app.get(JwtService);
-  const authService = new AuthService(prismaService, jwtService);
+  const authService = app.get(AuthService); 
   
   app.use(new AuthMiddleware(jwtService, authService).use);
   
